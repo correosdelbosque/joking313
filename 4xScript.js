edca5f2252d952dfee688af689a2d92b4b541f76
Joking313 <35207683+Joking313@users.noreply.github.com>
2018-01-10 20:42:19 -0500

    Update 4xScript.js

--- 4xScript.js
+++ 4xScript.js
@@ -1,7 +1,6 @@
 //You can change these variables:
 var baseBet = 1;//bet to return to on win
 var maxLoses = 4;//the number of losses you can take in a row
-//this means you will need baseBet*4^(maxLoses-1) bits
 
 //You can change these variables but it is recommended to leave them as is:
 var baseCashout = 1.04;//this is the cashout that will be returned to on a win, the cashout will be variable after a loss
@@ -11,24 +10,38 @@ var currentBet = baseBet;//used in determining what the current bet amount is
 var percent;//based on your baseBet and you maxLoses this will calculate what percent of your bits you will lose if you suffer more than "maxLoses" in a row
 //this will be used to find out by how much to proportionally increase your bet while remaining under the original percent
 var currentCashout = baseCashout;//used in determining what the current cashout is
-var betTotal = baseBet*Math.pow(4,maxLoses-1);
-var stopScriptOnLoss = true;//will terminate script on a loss,do not change this for now will update soon
-var cumulativeLoss = 0;//on loss this is used to determine how much to bet at the given cashout to break back to even
+var maxBet = baseBet*Math.pow(4,maxLoses-1);
+var stopScriptOnLoss = true;//will stop the script in the event of "maxLoses" loses in a row
+var cumulativeLoss = 0;
 var playing = false;//will delay initial start by one game so that if script is ran between 'game_started' and 'game_crash' phase 
 //it will not prematurly increase bet if busts below "currentCashout"
+var totalLoss = baseBet;//This is the total amount you would lose after "maxLoses" loses in a row
 
-//function used to determine if user has enough funds for the strategy they selected
+//used to calculate totalLoss
+function calcTotalLoss(){
+	let temp = baseBet;
+	for(let i=0;i<maxLoses-1;i++){
+		temp = temp*4;
+		totalLoss += temp;
+	}
+}
+calcTotalLoss();
+
 function balCheck(){
 	let bal = (engine.getBalance()/100);
-	if(bal<betTotal){
-		console.log("Based on your parameters you need a minimum of " + betTotal + " to use this strategy, and you only have " + bal);
+	if(bal<totalLoss){
+		console.log("Based on your parameters you need a minimum of " + totalLoss + " to use this strategy, and you only have " + bal);
 		engine.stop();
 	}
-	else
-		percent = betTotal/bal;
+	else if(maxBet>100000){
+		console.log("RaiGames allows bets no larger than 100000 as of 1/10/18, and your strategy would try to place a bet of size " + maxBet + " as the last bet which is too large");
+		engine.stop();
+	}
+	else{
+		percent = totalLoss/bal;
+	}
 }
 balCheck();
-
 
 engine.on('game_starting', function(info) {
     engine.placeBet(currentBet*100, currentCashout*100);
