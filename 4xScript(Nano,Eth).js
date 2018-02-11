b1c1136d08ed4848df8b323947bb8fbf3ef9e69d
Joking313 <35207683+Joking313@users.noreply.github.com>
2018-02-10 19:02:33 -0500

    Update 4xScript(Nano,Eth).js
    Updated GUI and got rid of bug where if bet packet got lost and the bet was not placed the script will no longer increase bet if the game crashed below cashout target

--- 4xScript(Nano,Eth).js
+++ 4xScript(Nano,Eth).js
@@ -18,8 +18,6 @@ var currentBet;//used in determining what the current bet amount is
 var currentCashout = baseCashout;//used in determining what the current cashout is
 //var stopScriptOnLoss = true;//will stop the script in the event of "maxLosses" losses in a row
 var cumulativeLoss = 0;// the cumulative loss of the current loss streak
-var playing = false;//will delay initial start by one game so that if script is ran between 'game_started' and 'game_crash' phase 
-//it will not prematurly increase bet if busts below "currentCashout"
 var lossStreak = 0;//number of losses in a row
 var userBalance = engine.getBalance()/100;//the users balance
 var divTable = [];//holds elements to pass to the UI to update table results
@@ -113,19 +111,14 @@ function createDivTable(){
 createDivTable();
 
 engine.on('game_starting', function(info) {
-	if(playing){
-		console.log("Current balance: " + engine.getBalance() + " will bet " + currentBet + " at " + currentCashout);
-		engine.placeBet(currentBet*100, currentCashout*100);
-	}
+	console.log("Current balance: " + engine.getBalance() + " will bet " + currentBet + " at " + currentCashout);
+	engine.placeBet(currentBet*100, currentCashout*100);
 });
 
 engine.on('game_crash', function(data) {
-	if(!playing){
-		playing = true;
-		console.log("Game start!");
+	if(engine.lastGamePlay()=='NOT_PLAYED')
 		return;
-	}
-	if((data.game_crash/100)<currentCashout){
+	if(engine.lastGamePlay()=='LOST'){
 		cumulativeLoss+=currentBet;
 		currentBet*=4;
 		currentCashout = parseFloat(((cumulativeLoss/currentBet)+1).toFixed(2));
@@ -245,17 +238,6 @@ function createTable(matrix){
 	header.innerHTML = "Script Stats";
 	header.style.borderRadius = "10px 10px 0px 0px";
 	header.style.cursor = "move";
-	let close = document.createElement("p");
-	close.innerHTML = "&#9746;";
-	close.style.display = "inline";
-	close.style.position = "absolute";
-	close.style.right = "10px";
-	close.style.cursor = "pointer";
-	header.appendChild(close);
-	close.addEventListener("click", function(){
-		document.body.removeChild(div);
-		engine.stop();
-	});
 	div.appendChild(header);
 	
 	let table = document.createElement("TABLE");
@@ -365,6 +347,11 @@ function createTable(matrix){
 	
 	document.body.appendChild(div);
 	
+	document.getElementsByClassName("strategy-stop")[0].addEventListener("click",function inner(){
+		document.body.removeChild(div);
+		document.getElementsByClassName("strategy-stop")[0].removeEventListener("click",inner);
+	});
+	
 	dragElement(div,header);
 }
 createTable(divTable);
@@ -434,10 +421,15 @@ function createDonateBox(){
 	para.style.padding = "5px";
 	para.style.maxWidth = "350px";
 	para.style.borderRadius = "0px 0px 10px 10px";
-	para.innerHTML = "All my scripts are availible free to use, however it you would like to show your support for my scripts feel free to send me tips to the account Joking313 on BustaBit, Raigames, and Ethcrash"
+	para.innerHTML = "All my scripts are availible free to use, however it you would like to show your support for my scripts feel free to send me tips to the account Joking313 on BustaBit, Raigames, and Ethcrash. I appreciate it :)";
 	div.appendChild(para);
 	
 	dragElement(div,header);
+	
+	document.getElementsByClassName("strategy-stop")[0].addEventListener("click",function inner(){
+		document.body.removeChild(div);
+		document.getElementsByClassName("strategy-stop")[0].removeEventListener("click",inner);
+	});
 	
 	document.body.appendChild(div);
 }
@@ -482,6 +474,11 @@ function createContactBox(){
 	div.appendChild(para);
 	
 	dragElement(div,header);
+	
+	document.getElementsByClassName("strategy-stop")[0].addEventListener("click",function inner(){
+		document.body.removeChild(div);
+		document.getElementsByClassName("strategy-stop")[0].removeEventListener("click",inner);
+	});
 	
 	document.body.appendChild(div);
 }
@@ -578,5 +575,11 @@ function createHelpBox(){
 	div.appendChild(last);
 	
 	dragElement(div,header);
+	
+	document.getElementsByClassName("strategy-stop")[0].addEventListener("click",function inner(){
+		document.body.removeChild(div);
+		document.getElementsByClassName("strategy-stop")[0].removeEventListener("click",inner);
+	});
+	
 	document.body.appendChild(div);
 }
